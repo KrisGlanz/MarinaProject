8b27bbc188aaadd8ad2f5afadd80f8528ad312de
Jacob Erven-Smalley <je719413@southeast.edu>
2023-02-06 14:40:20 -0600

    Add Customer Model

---
+++ MarinaProject/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarinaProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int customerId { get; set; }
+
+        [Required(ErrorMessage = "Please enter first name.")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string firstName { get; set; }
+        
+        [Required(ErrorMessage = "Please enter last name.")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string lastName { get; set; }
+        
+        [Required(ErrorMessage = "Please enter address.")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string address { get; set; }
+
+        [Required(ErrorMessage = "Please enter phone number.")]
+        [Column(TypeName = "int(10)")]
+        public int phoneNum { get; set; }
+
+        public string leaseType { get; set; }
+
+
+    }
+}
