d24fee3a63f3456dd454f6f83e68cba7105cf4d5
kg412051 <kristopherglanz@gmail.com>
2023-02-06 14:42:27 -0600

    added Boat model/controller

---
+++ MarinaProject/Models/Boat.cs
@@ -0,0 +1,12 @@
+﻿namespace MarinaProject.Models
+{
+    public class Boat
+    {
+        public int BoatId { get; set; }
+        public int CustomerId { get; set; }
+        public int BoatType { get; set; }
+        public Boolean Registration { get; set; }
+        public int BoatLength { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
